28de90e0e1905a5f8b46b28184aa5b7b8954b432
Adi Priyanto <adipriyanto.bpn@gmail.com>
2017-09-03 20:10:09 +0700

    prepare Bootstrap class

---
+++ Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace adipriyantobpn\composer;
+
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+/**
+ * Class Bootstrap
+ * @package adipriyantobpn\composer
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+
+}
